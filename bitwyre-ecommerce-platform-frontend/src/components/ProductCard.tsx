5086f59f1022ed22ebcf544b1acbf6d1f496a328
Akshat <akshat@Srishtis-MacBook-Air.local>
2024-11-18 20:45:03 +0530

    safari prodcut card align changes

--- bitwyre-ecommerce-platform-frontend/src/components/ProductCard.tsx
+++ bitwyre-ecommerce-platform-frontend/src/components/ProductCard.tsx
@@ -8,12 +8,14 @@ const ProductCard = ({product}:{product:productsType}) => {
     const currency = '$'
     const darkMode = useRecoilValue(darkModeAtom)
   return (
-    <NavLink to={`/product/${product._id}`} className={`${darkMode && 'text-white'}`}>
+    <NavLink to={`/product/${product._id}`}>
+      <div className={`w-full h-full ${darkMode && 'text-white'} flex flex-col`}>
         <div className='overflow-hidden h-[80%]'>
             <img className="hover:scale-110 transition-all w-full h-full" src={(product.image)[0]} alt="product-image" loading='lazy' />
         </div>
         <div className='text-sm leading-4 h-[13%]'>{product.name}</div>
         <p className='text-sm h-[7%]'>{`${currency} ${product.price}`}</p>
+      </div>
     </NavLink>
   )
 }
